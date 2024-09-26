77cc2aa4d3c6eb1753785c2d3433475c72c78907
amarinolama <amarinolama@gmail.com>
2024-09-26 08:31:31 +0200

    feat() : añadir método pasar xml a json

---
+++ src/main/java/edu/badpals/pokemon/XMLtoJSON.java
@@ -0,0 +1,12 @@
+package edu.badpals.pokemon;
+
+import com.fasterxml.jackson.databind.util.JSONPObject;
+
+import static com.sun.org.apache.xml.internal.security.c14n.implementations.CanonicalizerBase.XML;
+
+
+public class XMLtoJSON {
+    JSONPObject json = XML.toJSONOBJECT(string aa);
+    XMLtoJSON xml = new XMLtoJSON();
+
+}

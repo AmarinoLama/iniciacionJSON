a0fb786a02ad196cad57f40c17b08607d188670f
amarinolama <amarinolama@gmail.com>
2024-09-26 08:32:27 +0200

    feat() : añadir comentario

--- src/main/java/edu/badpals/pokemon/XMLtoJSON.java
+++ src/main/java/edu/badpals/pokemon/XMLtoJSON.java
@@ -4,9 +4,10 @@ import com.fasterxml.jackson.databind.util.JSONPObject;
 
 import static com.sun.org.apache.xml.internal.security.c14n.implementations.CanonicalizerBase.XML;
 
+// Escribir todo el fichero xml en un string y convertirlo a JSON
 
 public class XMLtoJSON {
-    JSONPObject json = XML.toJSONOBJECT(string aa);
+    //JSONPObject json = XML.toJSONOBJECT(string aa);
     XMLtoJSON xml = new XMLtoJSON();
 
 }
